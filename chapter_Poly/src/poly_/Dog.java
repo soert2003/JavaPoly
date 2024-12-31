cf79cdce624635b0906b65bc8272416d90ca8d7f
unknown <zenglingsen68@gmail.com>
2024-12-31 23:16:59 +0800

    first try

---
+++ chapter_Poly/src/poly_/Dog.java
@@ -0,0 +1,20 @@
+package poly_;
+
+/**
+ * @author:Sam Zeng
+ * @version:1.0
+ */
+public class Dog {
+    private String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ chapter_Poly/src/poly_/poly01.java
@@ -0,0 +1,11 @@
+package poly_;
+
+/**
+ * @author:Sam Zeng
+ * @version:1.0
+ */
+public class poly01 {
+    public static void main(String[] args) {
+
+    }
+}
